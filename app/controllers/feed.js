3f76f9880fa69aafdae26f3f4db758d82986ec1f
petercharlesxp <petercharles.ca@hotmail.com>
2016-02-24 16:25:10 -0500

    Figure 5-16: ios for the Camera Button

--- app/controllers/feed.js
+++ app/controllers/feed.js
@@ -9,3 +9,56 @@ OS_IOS && $.cameraButton.addEventListener("click",function(_event){
 $.cameraButtonClicked = function(_event) {
   alert("user clicked camera button");
 };
+
+$.cameraButtonClicked = function(_event) {
+   alert("user clicked camera button");
+
+   var photoSource = Titanium.Media.getIsCameraSupported() ? 
+       Titanium.Media.showCamera : Titanium.Media.openPhotoGallery;
+
+photoSource ({
+      success : function(event) {
+          processImage(event.media, function(_photoResp){
+              //photoObject = _photoResp;
+                            
+		      // create the row
+		      var row = Alloy.createController("feedRow", photoResp);
+		
+		      // add the controller view, which is a row to the table
+		      if ($.feedTable.getData().length === 0) {
+		         $.feedTable.setData([]);
+		         $.feedTable.appendRow(row.getView(), true);
+		      } else {
+		           $.feedTable.insertRowBefore(0,row.getView(), true);
+		        }
+          });
+      },
+      cancel : function() {
+       // called when user cancels taking a picture
+      },
+      error : function(error) {
+       // display alert on error
+          if (error.code == Titanium.Media.NO_CAMERA) {
+             alert('Please run this test on device');
+          } else {
+             alert('Unexpected error: ' + error.code);
+          }
+       },
+       saveToPhotoGallery : false,
+       allowEditing : true,
+       // only allow for photos, no video
+       mediaTypes : [Ti.Media.MEDIA_TYPE_PHOTO]
+   });
+};
+
+
+function processImage(_mediaObject, _callback) {
+  // since there is no ACS integration yet, we will fake it
+  var photoObject = {
+    image : _mediaObject,
+    title : "Sample Photo " + new Date()
+  };
+
+  // return the object to the caller
+  _callback(photoObject);
+}
--- app/controllers/feedRow.js
+++ app/controllers/feedRow.js
@@ -1,2 +1,8 @@
 // Arguments passed into this controller can be accessed off of the `$.args` object directly or:
-var args = $.args;
+//var args = $.args;
+var args = arguments[0] || {};
+
+// this is setting the view elements of the row view
+// based on the arguments passed into the controller
+$.image.image = args.image;
+$.titleLabel.text = args.title || '';
