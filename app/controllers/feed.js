d8bb50c03a75e813e7e7d158e6a3fba83fc74494
petercharlesxp <petercharles.ca@hotmail.com>
2016-02-24 13:49:34 -0500

    Figure 5-14: Adding the FeedRow View

--- app/controllers/feed.js
+++ app/controllers/feed.js
@@ -1,2 +1,11 @@
 // Arguments passed into this controller can be accessed off of the `$.args` object directly or:
 var args = $.args;
+
+OS_IOS && $.cameraButton.addEventListener("click",function(_event){
+    $.cameraButtonClicked(_event);
+});
+
+// handlers
+$.cameraButtonClicked = function(_event) {
+  alert("user clicked camera button");
+};
